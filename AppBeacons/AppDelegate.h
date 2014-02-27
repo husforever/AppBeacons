a78d1634a4aea7e4b2ed2ace33aedb0d186df89c
Sara Subijana Gracia <ssubijana@autentia.com>
2014-02-27 17:40:17 +0100

    Initial Commit

---
+++ AppBeacons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppBeacons
+//
+//  Created by Sara Subijana Gracia on 27/02/14.
+//  Copyright (c) 2014 Autentia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppBeacons/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  AppBeacons
+//
+//  Created by Sara Subijana Gracia on 27/02/14.
+//  Copyright (c) 2014 Autentia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
